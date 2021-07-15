b51153ebaeb646eb3db59342d93ca9da6ba40fc1
戈小荷 <gehongyan1996@126.com>
2021-07-15 15:31:36 +0800

    添加项目文件。

---
+++ KeggParser/MainWindow.xaml.cs
@@ -0,0 +1,189 @@
+﻿using KeggParser.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace KeggParser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 单链接执行处理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void ParseButton_Click(object sender, RoutedEventArgs e)
+        {
+            // 禁用按钮避免重复按下
+            ParseButton.IsEnabled = false;
+            ParsePatchButton.IsEnabled = false;
+
+            try
+            {
+                // 解析代码
+                string url = UrlTextBox.Text;
+                List<string> codeList = Utils.KeggParser.ParseLinkToCodes(url);
+
+                // 创建异步任务列表
+                List<Task<(string, string)>> tasks = codeList.Select(Utils.KeggParser.ParseCodeToSequenceNumberAsync).ToList();
+
+                while (tasks.Any())
+                {
+                    // 当有任务完成时
+                    Task<(string, string)> completedTask = await Task.WhenAny(tasks);
+                    tasks.Remove(completedTask);
+
+                    (string code, string seq) = await completedTask;
+
+                    // 打印结果
+                    ResultTextBox.Text += $"{code},{seq}\n";
+                }
+
+                ResultTextBox.Text += "===============================\n";
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.StackTrace);
+            }
+
+            // 恢复按钮禁用
+            ParseButton.IsEnabled = true;
+            ParsePatchButton.IsEnabled = true;
+        }
+
+        /// <summary>
+        /// 批量链接处理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void ParsePatchButton_Click(object sender, RoutedEventArgs e)
+        {
+            ParseButton.IsEnabled = false;
+            ParsePatchButton.IsEnabled = false;
+            try
+            {
+                // 提取代码列表
+                ResultTextBox.Text += "正在提取代码列表\n";
+                string url = UrlTextBox.Text;
+                List<PathwayStruct> list = await Utils.KeggParser.ParseCatalogAsync(url);
+
+                // 查找5200所在位置
+                int filteredIndex = list.FindIndex(p => p.Id == 5200);
+
+                // 滤除5200之后的条目，按数量排序，取前10多
+                List<PathwayStruct> tenMaxList =
+                    list.Take(filteredIndex)
+                        .OrderByDescending(p => p.Count)
+                        .Take(10)
+                        .ToList();
+
+                // 解析编码并合并
+                List<string> codeList = tenMaxList
+                    .SelectMany(p => Utils.KeggParser.ParseLinkToCodes(p.Url))
+                    .ToList();
+
+                int codeTotal = codeList.Count;
+                ResultTextBox.Text += $"获得代码共{codeTotal}个\n";
+
+                // 编码去重
+                codeList = codeList.Distinct().ToList();
+                int codeDistinct = codeList.Count;
+                ResultTextBox.Text += $"代码去重共{codeDistinct}个\n";
+
+                // 分组，每组10个，避免上百请求同时发起
+                ResultTextBox.Text += $"正在分组拉取结果\n";
+                List<List<string>> codeSeparated = new();
+
+                int j = 10;
+                for (int i = 0; i < codeList.Count; i += 10)
+                {
+                    List<string> cList = codeList.Take(j).Skip(i).ToList();
+                    j += 10;
+                    codeSeparated.Add(cList);
+                }
+
+                // 代码与编码结果字典
+                Dictionary<string, string> dic = new();
+
+                for (int index = 0; index < codeSeparated.Count; index++)
+                {
+                    // 解析编码到序号
+                    ResultTextBox.Text += $"第{index + 1}组/共{codeSeparated.Count}组\n";
+                    List<Task<(string, string)>> tasks = codeSeparated[index].Select(Utils.KeggParser.ParseCodeToSequenceNumberAsync).ToList();
+
+                    while (tasks.Any())
+                    {
+                        Task<(string, string)> completedTask = await Task.WhenAny(tasks);
+                        tasks.Remove(completedTask);
+
+                        try
+                        {
+                            // 添加字典
+                            (string code, string seq) = await completedTask;
+                            bool succeed = dic.TryAdd(code, seq);
+                            if (!succeed)
+                            {
+                                ResultTextBox.Text += $"向字典添加{code},{seq}失败\n";
+                            }
+                        }
+                        catch (Exception exception)
+                        {
+                            ResultTextBox.Text += $"查询过程中出现错误，请在检查输出结果\n";
+                            MessageBox.Show(exception.StackTrace);
+                        }
+                    }
+                }
+                ResultTextBox.Text += $"结果拉取完成，正在输出结果\n";
+
+                // 前十条重解析解析
+                foreach (PathwayStruct pathwayStruct in tenMaxList)
+                {
+                    ResultTextBox.Text += $"页面1：{pathwayStruct.Id:D5} ({pathwayStruct.Url})\n";
+                    ResultTextBox.Text += $"共{pathwayStruct.Count}条\n";
+
+                    List<string> codes = Utils.KeggParser.ParseLinkToCodes(pathwayStruct.Url);
+
+                    // 查找字典
+                    foreach (string code in codes)
+                    {
+                        if (dic.ContainsKey(code))
+                        {
+                            if (!string.IsNullOrWhiteSpace(dic[code]))
+                            {
+                                ResultTextBox.Text += $"{code},{dic[code]}\n";
+                            }
+                            else
+                            {
+                                ResultTextBox.Text += $"拉取{code}对应结果匹配空，请手动核实 https://www.kegg.jp/entry/{code}\n";
+                            }
+                        }
+                        else
+                        {
+                            ResultTextBox.Text += $"拉取{code}对应结果未成功，请手动核实 https://www.kegg.jp/entry/{code}\n";
+                        }
+
+                    }
+                }
+
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.StackTrace);
+            }
+            ParseButton.IsEnabled = true;
+            ParsePatchButton.IsEnabled = true;
+
+        }
+
+    }
+}
---
+++ KeggParser/Utils/KeggParser.cs
@@ -0,0 +1,116 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace KeggParser.Utils
+{
+    public static class KeggParser
+    {
+        /// <summary>
+        /// 解析链接到编码列表
+        /// </summary>
+        /// <param name="link">链接</param>
+        /// <returns>编码列表</returns>
+        public static List<string> ParseLinkToCodes(string link)
+        {
+            return link.Remove(0, 88).Split('/').Distinct().ToList();
+        }
+
+        /// <summary>
+        /// 异步解析编码代码到顺序号
+        /// </summary>
+        /// <param name="code">
+        /// <para>编码</para>
+        /// <example>K12345</example>
+        /// </param>
+        /// <returns>
+        /// <para>顺序号</para>
+        /// <example>1.2.3.4</example>
+        /// </returns>
+        public static async Task<(string, string)> ParseCodeToSequenceNumberAsync(string code)
+        {
+            // 文件存在则返回本地缓存
+            string filePath = Path.Combine(AppContext.BaseDirectory, "Data", $"{code}.data");
+            if (File.Exists(filePath))
+            {
+                string localData = await File.ReadAllTextAsync(filePath);
+                return (code, localData);
+            }
+
+            // 否则联网下载存储后返回
+            HtmlWeb webpage = new();
+            HtmlDocument webDoc = await webpage.LoadFromWebAsync($"https://www.kegg.jp/entry/{code}");
+            string docHtml = webDoc.DocumentNode.InnerText;
+
+            // 匹配形如 [EC:1.2.3.4] 的内容
+            Regex rgx = new(@"\[EC:((\d+\.){3}\d+)\]");
+            Match result = rgx.Match(docHtml);
+            string sequence = result.Groups[1].Value;
+
+            // 写本地缓存
+            await File.WriteAllTextAsync(filePath, sequence);
+
+            return (code, sequence);
+        }
+
+        /// <summary>
+        /// 解析数量
+        /// </summary>
+        /// <param name="text">文本条目</param>
+        /// <returns>末尾括号中的数据量</returns>
+        private static int ParseCount(string text)
+        {
+            Regex rgx = new(@"\((\d+)\)");
+            Match match = rgx.Match(text);
+            string count = match.Groups[1].Value;
+            return int.Parse(count);
+        }
+
+        /// <summary>
+        /// 解析目录页到页面列表
+        /// </summary>
+        /// <param name="url">目录页地址</param>
+        /// <returns>页面列表</returns>
+        public static async Task<List<PathwayStruct>> ParseCatalogAsync(string url)
+        {
+            HtmlWeb webpage = new();
+            HtmlDocument webDoc = await webpage.LoadFromWebAsync(url);
+            HtmlNodeCollection nodes = webDoc.DocumentNode.SelectNodes(@"//*[@id=""main""]/p/a");   // XPath
+
+            List<PathwayStruct> list = nodes.Select(n => new PathwayStruct()
+            {
+                Url = n.Attributes["href"].Value,
+                Count = ParseCount(n.NextSibling.InnerText),
+                Id = int.Parse(n.InnerText)
+            }).ToList();
+            return list;
+        }
+    }
+
+
+    /// <summary>
+    /// Pathway结构
+    /// </summary>
+    public struct PathwayStruct
+    {
+        /// <summary>
+        /// 网址
+        /// </summary>
+        public string Url;
+
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int Id;
+
+        /// <summary>
+        /// 数量
+        /// </summary>
+        public int Count;
+    }
+
+}
